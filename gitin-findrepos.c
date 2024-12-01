e5556f6cb51978e61195c2ef027b952cf159abfd
Friedel Schon <derfriedmundschoen@gmail.com>
2024-12-01 18:41:26 +0100

    fiadd -C to findrepos

--- gitin-findrepos.c
+++ gitin-findrepos.c
@@ -1,26 +1,48 @@
-
+#include "arg.h"
 #include "config.h"
 #include "findrepo.h"
 
 #include <stdlib.h>
+#include <unistd.h>
 
 
 int archivezip, archivetargz, archivetarxz, archivetarbz2; /* otherwise we have linker-errors */
 
+static __attribute__((noreturn)) void usage(int exitcode) {
+	fprintf(stderr, "usage: gitin-findrepos [-C chdir] [start]\n");
+	exit(exitcode);
+}
+
 int main(int argc, char* argv[]) {
 	char** repos  = NULL;
 	int    nrepos = 0;
+	char*  start;
 
-	if (argc == 1) {
-		findrepos(".", &repos, &nrepos);
-	} else if (argc == 2) {
-		findrepos(argv[0], &repos, &nrepos);
+	ARGBEGIN
+	switch (OPT) {
+		case 'C':
+			chdir(EARGF(usage(1)));
+			break;
+		case 'h':
+			usage(0);
+		default:
+			fprintf(stderr, "error: unknown option '%d'\n", OPT);
+			exit(1);
+	}
+	ARGEND
+
+	if (argc == 0) {
+		start = ".";
+	} else if (argc == 1) {
+		start = argv[0];
 	} else {
-		fprintf(stderr, "usage: gitin-findrepos [start]\n");
-		return 1;
+		fprintf(stderr, "error: too many arguments\n");
+		usage(1);
 	}
 
-	if (nrepos) {
+	findrepos(start, &repos, &nrepos);
+
+	if (!nrepos) {
 		fprintf(stderr, "no repository found.\n");
 		return 1;
 	}
